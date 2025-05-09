3ef17400ff2514962cb10353e0a9f8bba3fbeea0
mfrickss <mfricks777@gmail.com>
2025-05-09 16:10:18 -0300

    Começando a API e alterando alguns arquivos inutilizados

---
+++ MinimalApi/Titan.cs
@@ -0,0 +1,11 @@
+namespace MinimalApi
+{
+    public class Titan
+    {
+        public int id { get; set; }
+        public string? nome { get; set; }
+        public string? portador { get; set; }
+        public int idadePortador { get; set; }
+
+    }
+}
